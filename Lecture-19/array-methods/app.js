18beed8cb68942ec99ed17e47f2398c4ca1bcda5
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-11-29 02:27:02 +0530

    lecture 19 codes

---
+++ Lecture-19/array-methods/app.js
@@ -0,0 +1,37 @@
+
+
+
+
+let arr = [10,20,30,40,50];
+// 1. length
+// console.log(arr.length)
+
+// 2. push()
+
+// let returnType = arr.push("sam");
+// let returnType = arr.push("sam" ,20, false); //multiple valid
+// console.log(returnType); //length
+// console.log(arr)
+
+// 3. pop()
+
+// arr.pop(50);
+// arr.pop(40);
+// let returnType = arr.pop();
+// console.log(returnType)
+// console.log(arr);
+
+// 4. unshift() -> push()
+
+// let returnType =  arr.unshift('monu bhaiya ki shadi');
+// let returnType =  arr.unshift('monu', 'bhaiya', 'ki', 'shadi');
+
+// console.log(returnType)
+// console.log(arr)
+
+
+// 5.shift() -> pop()
+
+let returnType = arr.shift();
+console.log(returnType)
+console.log(arr)
---
+++ Lecture-19/arrays/app.js
@@ -0,0 +1,28 @@
+
+
+// let arr = [10,20,"sam" , false, "undefined"]
+
+
+// console.log(arr);
+
+// console.log(arr[3])
+
+
+// -----------------------------------
+
+// let arr2 = [10,20,[100,200, [6000] , 0], "sam" , true]
+
+
+// console.log(arr2[1])
+// console.log(arr2[2])
+// console.log(arr2[2][1])
+// console.log(arr2[2][2])
+
+
+// console.log( arr2[2][2][0]  )
+
+// -----------------------------------
+
+// array methods
+
+
