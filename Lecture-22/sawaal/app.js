0d4d4fb7fbcc2461adc831ab1e7f382a4ce06fec
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-12-06 22:00:53 +0530

    lecture 22 codes

---
+++ Lecture-22/sawaal/app.js
@@ -0,0 +1,50 @@
+// const a = 10;
+// a = 100
+
+
+// -----------------
+
+// const a;
+// a = 100;
+
+// -----------------
+
+// var apnaNaam = "shukla";
+// var a = 100;
+// function kajuMoyeMoye(){
+//     console.log(a);
+//     var c = 20;
+//     console.log(c);
+// }
+
+// kajuMoyeMoye();
+// console.log(apnaNaam);
+
+// -----------------------
+
+// var a = 100;
+// let b = 200;
+// function abraKaDabra(){
+//     var c = 300;
+//     let d = 400;
+//     console.log(c)
+//     console.log(d)
+// }
+// abraKaDabra();
+// console.log(a)
+// console.log(b)
+
+// -----------------------
+
+var a = 100;
+let b = 200;
+
+if(a == '100'){
+    var a = 300;
+    let b = 400;
+    console.log(a)
+    console.log(b)
+}
+
+console.log(a)
+console.log(b)
