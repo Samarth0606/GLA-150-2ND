b32876626455146a8c6a6aa8204581c8feae0aa7
Samarth Vohra <officialdevelopersamarth@gmail.com>
2024-01-24 16:24:59 +0530

    lecture 25 codes

---
+++ Lecture-25/constructor/app.js
@@ -0,0 +1,14 @@
+// function sam() {}
+
+// let out = sam();
+// console.log(out);
+
+// ----------------------
+//constructor function
+function Sam() {
+  this.naam = "kaju";
+  this.umar = 2;
+}
+
+let out = new Sam();
+console.log(out);
---
+++ Lecture-25/proto-type/app.js
@@ -0,0 +1,18 @@
+let obj = {
+  a: 10,
+  b: "samarth",
+  c: function () {
+    console.log("samarth vohra");
+  },
+};
+
+let arr = [10, 20, 30];
+
+let str = "samarth vohra";
+
+// console.log(obj.b);
+// console.log(obj.c());
+
+// console.log(obj.d);
+// // console.log(obj.e());
+// console.log(obj.toString());
