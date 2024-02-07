5879b734511587cfd2ce0648073104a06d5f7778
Samarth Vohra <officialdevelopersamarth@gmail.com>
2024-02-07 22:22:10 +0530

    lecture 27 codes

---
+++ Lecture-27/Async/app.js
@@ -0,0 +1,39 @@
+// console.log("start");
+
+// console.log("after 4 secs");
+
+// console.log("end");
+
+// --------------------------
+
+// console.log("start");
+
+// setTimeout(function () {
+//   console.log("after 4 seconds");
+// }, 4 * 1000);
+
+// console.log("end");
+
+// --------------------------
+
+// console.log("hi rose");
+
+// setTimeout(() => {
+//   console.log("after 4  secs");
+// }, 4000);
+
+// setTimeout(() => {
+//   console.log("after 3 more secs");
+// }, 3000);
+
+// console.log("end");
+
+// --------------------
+
+// console.log("start");
+
+// setTimeout(() => {
+//   console.log("after 0 more secs");
+// }, 0);
+
+// console.log("end");
